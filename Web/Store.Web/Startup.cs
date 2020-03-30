a4e8aa8f9e1e287fc7345145b5ba54b480b3678c
airfanBG <damyanovdaniel@yahoo.com>
2020-03-30 23:57:57 +0300

    Home page template added

--- Web/Store.Web/Startup.cs
+++ Web/Store.Web/Startup.cs
@@ -55,6 +55,7 @@ namespace Store.Web
             });
             services.AddAuthorization();
             services.AddControllersWithViews();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
